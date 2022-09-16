53c0de2deedda914a8f4775b2ab1ce82df58c7cb
MeanNapat012 <110965346+MeanNapat012@users.noreply.github.com>
2022-09-16 12:29:02 +0700

    Update UserWork.cs

--- quiz/UserWork.cs
+++ quiz/UserWork.cs
@@ -12,11 +12,11 @@ class UserWork
     public void AllWork()
     {
         Console.WriteLine("Nameworker");
-        Console.WriteLine("Work : {0} Name : {1} Surname : {2} Age : {3} Money : {4}",work[0],nameWork[0],surnameWork[0],age[0],moneywork[0]);
-        Console.WriteLine("Work : {0} Name : {1} Surname : {2} Age : {3} Money : {4}",work[0],nameWork[1],surnameWork[1],age[1],moneywork[0]);
-        Console.WriteLine("Work : {0} Name : {1} Surname : {2} Age : {3} Money : {4}",work[0],nameWork[2],surnameWork[2],age[2],moneywork[0]);
-        Console.WriteLine("Work : {0} Name : {1} Surname : {2} Age : {3} Money : {4}",work[1],nameWork[3],surnameWork[3],age[3],moneywork[1]);
-        Console.WriteLine("Work : {0} Name : {1} Surname : {2} Age : {3} Money : {4}",work[1],nameWork[1],surnameWork[4],age[4],moneywork[1]);
+        Console.WriteLine("Work : {0} / Name : {1} / Surname : {2} / Age : {3} / Money : {4}",work[0],nameWork[0],surnameWork[0],age[0],moneywork[0]);
+        Console.WriteLine("Work : {0} / Name : {1} / Surname : {2} / Age : {3} / Money : {4}",work[0],nameWork[1],surnameWork[1],age[1],moneywork[0]);
+        Console.WriteLine("Work : {0} / Name : {1} / Surname : {2} / Age : {3} / Money : {4}",work[0],nameWork[2],surnameWork[2],age[2],moneywork[0]);
+        Console.WriteLine("Work : {0} / Name : {1} / Surname : {2} / Age : {3} / Money : {4}",work[1],nameWork[3],surnameWork[3],age[3],moneywork[1]);
+        Console.WriteLine("Work : {0} / Name : {1} / Surname : {2} / Age : {3} / Money : {4}",work[1],nameWork[1],surnameWork[4],age[4],moneywork[1]);
 
 
     }
